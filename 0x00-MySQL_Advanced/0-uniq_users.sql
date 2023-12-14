92d82492bf2f6d47cacc4bfe0b5bb1cf0b7c24ab
Stanhope Chinonso <stanhope.c.arthur@gmail.com>
2023-12-14 23:47:03 +0100

    0. We are all unique!

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,8 @@
+-- Creates a table with unique users email address.
+
+CREATE TABLE IF NOT EXISTS
+    users (
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        email VARCHAR (255) NOT NULL UNIQUE,
+        name VARCHAR (255)
+    );
